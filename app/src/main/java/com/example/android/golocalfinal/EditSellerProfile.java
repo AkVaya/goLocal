1e7f4b8f75da7da0754fd7c8025fe1c9fbca0c5b
Vasu Sehgal <vasu.18je0907@am.iitism.ac.in>
2020-10-16 14:02:45 +0530

    Issue with scrolling fixed and layout of products and cateories fixed

--- app/src/main/java/com/example/android/golocalfinal/EditSellerProfile.java
+++ app/src/main/java/com/example/android/golocalfinal/EditSellerProfile.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.android.golocalfinal.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
